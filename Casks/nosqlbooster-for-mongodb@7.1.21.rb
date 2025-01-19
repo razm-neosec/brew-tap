070a3b88b7ba2a1e1f12c002eecfd5a51dc8fc1c
Raz Magori <rmagori@akamai.com>
2025-01-19 16:20:53 +0200

    feat: Added version 7.1

---
+++ Casks/nosqlbooster-for-mongodb@7.1.21.rb
@@ -0,0 +1,24 @@
+cask "nosqlbooster-for-mongodb@7.1.21" do
+  version "8.1.9"
+  sha256 "08fe12d049fc3384ea106065012cf6eadd434354f2331a0998abde586a32ede7"
+
+  url "https://s3.nosqlbooster.com/download/releasesv#{version.major}/nosqlbooster4mongo-#{version}.dmg"
+  name "NoSQLBooster for MongoDB"
+  desc "GUI tool and IDE for MongoDB"
+  homepage "https://nosqlbooster.com/"
+
+  livecheck do
+    url "https://nosqlbooster.com/downloads"
+    regex(/%22patch_ver%22(?:%20|\s)*%3A(?:%20|\s)*%22v?(\d+(?:\.\d+)+)%22/i)
+  end
+
+  app "NoSQLBooster for MongoDB.app"
+
+  zap trash: [
+    "~/Documents/NoSQLBooster",
+    "~/Library/Application Support/NoSQLBooster for MongoDB",
+    "~/Library/Preferences/com.nosqlbooster.mongodb.plist",
+    "~/Library/Saved Application State/com.nosqlbooster.mongodb.savedState",
+  ]
+end
+
